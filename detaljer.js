44b5a83d9c8ff11c775ce5f41142db67b3a0f84c
Elise Fiskeseth <elise@pc-104-246.eduroam.uib.no>
2019-05-06 16:54:37 +0200

    hehe

--- detaljer.js
+++ detaljer.js
@@ -26,7 +26,34 @@ function detaljer(kom) {
 
         for (i in kommunenamn){
                if (kommunenummer[i] == kom) {
-                  table = document.getElementById("detaljer2");
+                  var table = document.getElementById("detaljer2");
+                  var row = document.createElement("tr");
+                  var kommuneHeader = document.createElement("th");
+                  var kommunenrHeader = document.createElement("th");
+                  var befolkningHeader = document.createElement("th");
+                  var sysselsettingHeader = document.createElement("th");
+                  var sysselsettingantallHeader = document.createElement("th");
+                  var utdanningHeader = document.createElement("th");
+                  var utdanningantallHeader= document.createElement("th");
+
+                  kommuneHeader.innerHTML = "Kommune";
+                  kommunenrHeader.innerHTML = "Kommunenummer";
+                  befolkningHeader.innerHTML = "Befolkning";
+                  sysselsettingHeader.innerHTML = "Sysselsettings%";
+                  sysselsettingantallHeader.innerHTML = "Antall sysselsatte";
+                  utdanningHeader.innerHTML = "Utdannings%";
+                  utdanningantallHeader.innerHTML = "Antall med høyere utdanning";
+
+                  row.appendChild(kommuneHeader);
+                  row.appendChild(kommunenrHeader);
+                  row.appendChild(befolkningHeader);
+                  row.appendChild(sysselsettingHeader);
+                  row.appendChild(sysselsettingantallHeader);
+                  row.appendChild(utdanningHeader);
+                  row.appendChild(utdanningantallHeader);
+
+                  table.appendChild(row);
+
                   var result = document.createElement("tr");
                   var namn = document.createElement("td");
                   var nummer = document.createElement("td");
@@ -54,10 +81,32 @@ function detaljer(kom) {
                   result.appendChild(utdanningantal);
                   table.appendChild(result);
 
+                  //befolkningshistorikk
+                  var tabellBefolk= document.getElementById("befolkning");
+                  var row1 = document.createElement("tr");
+                  var row2 = document.createElement("tr");
+                  var årHeader = document.createElement("th");
+                  var kvinnerHeader = document.createElement("th");
+                  var mennHeader = document.createElement("th");
+                  var totalHeader = document.createElement("th");
+                  var befolkHeader = document.createElement("th");
 
+                  årHeader.innerHTML = "Årstall";
+                  kvinnerHeader.innerHTML = "Kvinner";
+                  mennHeader.innerHTML = "Menn";
+                  totalHeader.innerHTML = "Total befolkning";
+                  befolkHeader.innerHTML = "Befolkningshistorikk";
+
+                  row1.appendChild(befolkHeader);
+                  tabellBefolk.appendChild(row1);
+
+                  row2.appendChild(årHeader);
+                  row2.appendChild(kvinnerHeader);
+                  row2.appendChild(mennHeader);
+                  row2.appendChild(totalHeader);
+                  tabellBefolk.appendChild(row2);
 
                   for (år in befolk.getInfo(kom).Menn) {
-
                       //befolkning
                       var tabell = document.getElementById("befolkning");
                       var årstall = document.createElement("tr");
@@ -71,9 +120,36 @@ function detaljer(kom) {
                       befolkning.innerHTML =  temp.Kvinner[år] + 0 + temp.Menn[år];
                       årstall.innerHTML += "<td>" + kvinner.innerHTML + "</td>" + "<td>" + menn.innerHTML + "</td>" + "<td>"+ befolkning.innerHTML + "</td>";
                       tabell.appendChild(årstall);
+                    }
 
+
+                  //sysselhistorikk
+                  var tabellSyssel = document.getElementById("sysselsatte");
+                  var rad1 = document.createElement("tr");
+                  var rad2 = document.createElement("tr");
+                  var årHeader = document.createElement("th");
+                  var kvinnerHeader = document.createElement("th");
+                  var mennHeader = document.createElement("th");
+                  var totalHeader = document.createElement("th");
+                  var sysselHeader = document.createElement("th");
+
+                  årHeader.innerHTML = "Årstall";
+                  kvinnerHeader.innerHTML = "Kvinner";
+                  mennHeader.innerHTML = "Menn";
+                  totalHeader.innerHTML = "Totalt sysselsatte";
+                  sysselHeader.innerHTML = "Sysselsettingshistorikk";
+
+                  rad1.appendChild(sysselHeader);
+                  tabellSyssel.appendChild(rad1);
+
+                  rad2.appendChild(årHeader);
+                  rad2.appendChild(kvinnerHeader);
+                  rad2.appendChild(mennHeader);
+                  rad2.appendChild(totalHeader);
+                  tabellSyssel.appendChild(rad2);
+
+                  for (år in befolk.getInfo(kom).Menn) {
                       //sysselsetting
-                      var tabellSyssel = document.getElementById("sysselsatte");
                       var årstallSyssel = document.createElement("tr");
                       var sysselsatte = document.createElement("td");
                       var kvinnerSyssel = document.createElement("td");
@@ -83,11 +159,48 @@ function detaljer(kom) {
                       sysselsatte.innerHTML = syssel.getInfo(kom).Begge[år];
                       kvinnerSyssel.innerHTML = syssel.getInfo(kom).Kvinner[år];
                       mennSyssel.innerHTML = syssel.getInfo(kom).Menn[år];
-                      årstallSyssel.innerHTML += "<td>" + kvinnerSyssel.innerHTML + "</td>" +"<td>" + mennSyssel.innerHTML + "</td>" +"<td>" + sysselsatte.innerHTML + "</td>";
+                      årstallSyssel.innerHTML += "<td>" + kvinnerSyssel.innerHTML + "</td>" + "<td>" + mennSyssel.innerHTML + "</td>" + "<td>" + sysselsatte.innerHTML + "</td>";
                       tabellSyssel.appendChild(årstallSyssel);
                   }
 
+
+                  var tabellUtd = document.getElementById("sysselsatte");
+                  var rad1 = document.createElement("tr");
+                  var rad2 = document.createElement("tr");
+                  var årHeader = document.createElement("th");
+                  var grunnskoleHeader = document.createElement("th");
+                  var vgsHeader = document.createElement("th");
+                  var fagskoleHeader = document.createElement("th");
+                  var bachelorHeader = document.createElement("th");
+                  var masterHeader = document.createElement("th");
+                  var ingenUtdHeader = document.createElement("th");
+                  var utdHeader = document.createElement("th");
+
+
+                  årHeader.innerHTML = "Årstall";
+                  grunnskoleHeader.innerHTML = "Grunnskole";
+                  vgsHeader.innerHTML = "Videregående skole";
+                  fagskoleHeader.innerHTML = "Fagskole";
+                  bachelorHeader.innerHTML = "Bachelorgrad";
+                  masterHeader.innerHTML = "Mastergrad";
+                  ingenUtdHeader.innerHTML = "Ingen utdanning";
+                  utdHeader.innerHTML = "Utdanningshistorikk";
+
+                  rad1.appendChild(utdHeader);
+                  tabellUtd.appendChild(rad1);
+
+                  rad2.appendChild(årHeader);
+                  rad2.appendChild(grunnskoleHeader);
+                  rad2.appendChild(vgsHeader);
+                  rad2.appendChild(fagskoleHeader);
+                  rad2.appendChild(bachelorHeader);
+                  rad2.appendChild(masterHeader);
+                  rad2.appendChild(ingenUtdHeader);
+
+                  tabellUtd.appendChild(rad2);
+
                   for (år in befolk.getInfo(kom).Menn) {
+                    //utdanning
                     if(år > 2017){
                       break;
                     }
